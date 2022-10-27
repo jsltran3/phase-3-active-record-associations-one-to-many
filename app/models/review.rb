05066dba13c5f6b5d0f2deec26ff33106ca81e5e
Joseph <jsltran@gmail.com>
2022-10-27 15:00:55 -0700

    commit tree 1d76ac7c4e894c035f68be6c2e718939df20a36f

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,3 +1,10 @@
 class Review < ActiveRecord::Base
+  #a review belongs to a game
+  belongs_to :game
+  
+  #def game
+    # self is the review instance
+    #the argument is the review's identification of the game idin the class Game
+    # Game.find(self.game_id)
   
 end
